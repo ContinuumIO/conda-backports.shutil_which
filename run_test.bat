2122a2f19891c442baaafe5d3b228236ea4b36a2
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-23 21:21:40 -0400

    fix pkg name

--- run_test.bat
+++ run_test.bat
@@ -1,3 +1,3 @@
 "%PREFIX%\Scripts\pip" install pytest-cov
-"%PREFIX%\Scripts\py.test" -l --cov=nbdime --pyargs backports.shutil_which
+"%PREFIX%\Scripts\py.test" -l --cov=backports.shutil_which --pyargs backports.shutil_which
 if errorlevel 1 exit 1
