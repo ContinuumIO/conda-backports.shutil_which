a343d18a294948ff284c374f7d1d783c07582bfc
Nicholas Bollweg <nbollweg@continuum.io>
2016-03-23 21:27:11 -0400

    simplify

--- run_test.bat
+++
@@ -1,3 +0,0 @@
-"%PREFIX%\Scripts\pip" install pytest-cov
-"%PREFIX%\Scripts\py.test" -l --cov=backports.shutil_which --pyargs backports.shutil_which
-if errorlevel 1 exit 1
--- run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-"${PREFIX}/bin/pip" install pytest-cov
-"${PREFIX}/bin/py.test" -l --cov=backports.shutil_which --pyargs backports.shutil_which
